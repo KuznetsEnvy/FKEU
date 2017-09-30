96cdc68f62b085867e28d69d61ba62e72fb6feb3
DK <kuzznetss@yandex.ru>
2017-09-30 03:08:54 +0300

    1. Finally implemented new instagram integration 2. Removed some obsolete styles 3. Applied new header design 4. Reworked some translations 5. Applied new footer design 6. Added bestsellers block 7. Applied new design for products on main page

--- assets/main-belen.js
+++ assets/main-belen.js
@@ -10,26 +10,4 @@ $(window).on('load', function() {
     if ( $('.headerSub_limit').length > 0 ) {
         headerSub_limit = 54;
     }
-
-    console.log('invoking isotope');
-    console.log($('#insta .grid'));
-    $('#insta .grid').isotope({
-            itemSelector: '.grid-item',
-            percentPosition: true,
-            masonry: {
-                //columnWidth: 100,
-                isFitWidth: true
-            }
-    });
 });
-/*
-$(window).scroll( function() {
-  if ( st > headerSub_limit ) {
-    $('.js-header').addClass('is-stuck');
-  } else {
-    $('.js-header').removeClass('is-stuck');
-  }
-  st = $(this).scrollTop();
-});
-*/
-
